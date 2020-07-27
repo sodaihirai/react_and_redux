2e0c608c425f6bc126d34105b478b1a76e3cffe8
sodai.hirai <sodai.hirai@enigmo.co.jp>
2020-07-27 21:49:55 +0900

    react context

---
+++ translate/src/components/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
+
+class Button extends React.Component {
+  static contextType = LanguageContext;
+  render() {
+    const text = this.context === "english" ? "Submit" : "Voorleggen"
+    return <button className="ui primary">{text}</button>;
+  }
+}
+
+export default Button;
